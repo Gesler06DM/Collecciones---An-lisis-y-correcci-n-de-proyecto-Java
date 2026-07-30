273ddaefae56fef86e31b789c2ddf57a3cd41212
Gesler Duque <gduquem3@miumg.edu.gt>
2026-07-29 21:30:42 -0600

    tarea 2 completada

---
+++ clase02-colecciones/src/edu/umg/programacion2/clase02/ejercicios/asistencia/ControlAsistencia.java
@@ -0,0 +1,26 @@
+package edu.umg.programacion2.clase02.ejercicios.asistencia;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * TODO (estudiante): completar registrarAsistencia().
+ * <p>
+ * Regla de negocio: cada carnet solo puede registrar asistencia una vez por
+ * clase. El método debe indicar si el registro fue nuevo o si ya existía.
+ * <p>
+ * Pista: Set.add() ya retorna false cuando el elemento estaba repetido;
+ * no necesitas combinar contains() + add().
+ */
+public class ControlAsistencia {
+
+	private final Set<String> carnetsPresentes = new HashSet<>();
+
+	public boolean registrarAsistencia(String carnet) {
+		// TODO: reemplazar esta línea por carnetsPresentes.add(carnet).
+		return carnetsPresentes.add(carnet);
+	}
+	public int totalPresentes() {
+		return carnetsPresentes.size();
+	}
+}
